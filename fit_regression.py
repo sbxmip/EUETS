84603b9de6045fe9e3523aa0716231a1f8e9842c
michel <michel.philippens@sas.com>
2024-03-19 10:19:18 +0100

    Change backward selection into forward

--- fit_regression.py
+++ fit_regression.py
@@ -1,15 +1,11 @@
 import os
 import swat
 import getpass
-from sasctl import Session
-from sasctl.tasks import register_model, publish_model
 
 password = getpass.getpass ('Enter your password : ')
 hostname = "https://viya4-s2.zeus.sashq-d.openstack.sas.com/"
 os.environ["CAS_CLIENT_SSL_CA_LIST"] = r"C:\Users\sbxmip\.vscode\trustedcerts.pem"
 conn = swat.CAS('https://viya4-s2.zeus.sashq-d.openstack.sas.com/cas-shared-default-http', username='sbxmip',password=password)
-Session(hostname, 'sbxmip', password)
-
 EU_ETS_tbl = conn.CASTable('EU_ETS_CURATED', caslib='EBA')
 
 conn.loadActionSet('regression')
@@ -20,6 +16,6 @@ conn.regression.glm(
                    'effects':['Country_id', 'activity_desc', 'nace_desc']
                   },
     store = dict(name='reg_model', replace=True),
-    selection = {"method":"BACKWARD"}
+    selection = {"method":"FORWARD"}
 )
 
